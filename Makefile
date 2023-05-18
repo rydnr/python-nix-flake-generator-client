4f4966fa09dccff298585d7ee28dff3620625cd5
rydnr <github@acm-sl.org>
2023-05-18 12:00:53 +0200

    gRPC client now sends valid git_repo_found events

--- Makefile
+++ Makefile
@@ -5,5 +5,8 @@
 # @version 0.1
 .PHONY: grpc
 grpc:
-	python -m grpc_tools.protoc -I./protobufs --python_out=. --grpc_python_out=. ./protobufs/*.proto
+	python -m grpc_tools.protoc -I./protobufs --python_out=./src/infrastructure/network/grpc --grpc_python_out=./src/infrastructure/network/grpc ./protobufs/*.proto
+	@for file in src/infrastructure/network/grpc/*_pb2_grpc.py; do \
+		sed -i 's/import \([a-zA-Z_]*\)_pb2 as \([a-zA-Z_]*\)__pb2/import infrastructure.network.grpc.\1_pb2 as \2__pb2/g' $$file ; \
+	done
 # end
--- src/infrastructure/network/grpc/client.py
+++ src/infrastructure/network/grpc/client.py
@@ -1,9 +1,10 @@
 from domain.event import Event
 from domain.server import Server
 
+import infrastructure.network.grpc.git_repo_found_pb2 as git_repo_found_pb2
+import infrastructure.network.grpc.git_repo_found_pb2_grpc as git_repo_found_pb2_grpc
+
 import grpc
-import hello_pb2
-import hello_pb2_grpc
 import logging
 
 class Client(Server):
@@ -17,16 +18,14 @@ class Client(Server):
         """
         Sends the event to the remote gRPC server.
         """
-        logging.getLogger(__name__).debug(f"Sending Hello to server {self.__class__.server_url}")
+        logging.getLogger(__name__).debug(f"Sending event {event} to server {self.__class__.server_url}")
         # Create a channel to the server
         with grpc.insecure_channel(self.__class__._server_url) as channel:
             # Create a stub (client)
-            stub = hello_pb2_grpc.GreeterStub(channel)
+            stub = git_repo_found_pb2_grpc.GitRepoFoundServiceStub(channel)
 
-            # Create a HelloRequest message
-            hello_request = hello_pb2.HelloRequest(name='Hello')
+            # Create a GitRepoFound message
+            git_repo_found = git_repo_found_pb2.GitRepoFound(package_name=event.package_name, package_version=event.package_version, url=event.url, tag=event.tag)
 
-            # Use the stub to call the SayHello RPC
-            response = stub.SayHello(hello_request)
-
-            print("Greeter client received: " + response.message)
+            # Use the stub to notify of GitRepoFound events
+            response = stub.GitRepoFoundNotifications(git_repo_found)
